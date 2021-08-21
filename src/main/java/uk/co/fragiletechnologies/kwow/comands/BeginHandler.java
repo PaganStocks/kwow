9d7b81744395dad5b41544aadf4cf777abe7985b
pagan <pagan.stocks@gmail.com>
2021-08-21 15:17:24 +0100

    begin and give have message responses

--- src/main/java/uk/co/fragiletechnologies/kwow/comands/BeginHandler.java
+++ src/main/java/uk/co/fragiletechnologies/kwow/comands/BeginHandler.java
@@ -3,8 +3,10 @@ package uk.co.fragiletechnologies.kwow.comands;
 import discord4j.common.util.Snowflake;
 import discord4j.core.object.entity.Message;
 import discord4j.core.object.entity.channel.MessageChannel;
+import discord4j.core.spec.EmbedCreateSpec;
 import uk.co.fragiletechnologies.kwow.data.Player;
 import uk.co.fragiletechnologies.kwow.data.PlayersRepository;
+
 
 public class BeginHandler implements MessageHandler {
 
@@ -19,6 +21,7 @@ public class BeginHandler implements MessageHandler {
         Player entity = new Player();
         entity.setDiscordId(getAuthorId(message));
         playersRepository.createUser(entity);
+        messageChannel.createEmbed(this::createEmbedForBegin).block();
     }
 
     private Long getAuthorId(Message message) {
@@ -29,4 +32,11 @@ public class BeginHandler implements MessageHandler {
     public boolean supportsMessage(Message message) {
         return message.getContent().equalsIgnoreCase(PREFIX + "begin");
     }
+
+    public void createEmbedForBegin(EmbedCreateSpec embedCreateSpec){
+        embedCreateSpec.addField(" begin", "You have set up your user, you can now play the bot", true);
+    }
+
 }
+
+
--- src/main/java/uk/co/fragiletechnologies/kwow/comands/GiveHandler.java
+++ src/main/java/uk/co/fragiletechnologies/kwow/comands/GiveHandler.java
@@ -4,6 +4,7 @@ import discord4j.common.util.Snowflake;
 import discord4j.core.object.entity.Message;
 import discord4j.core.object.entity.User;
 import discord4j.core.object.entity.channel.MessageChannel;
+import discord4j.core.spec.EmbedCreateSpec;
 import uk.co.fragiletechnologies.kwow.data.PlayersRepository;
 import uk.co.fragiletechnologies.kwow.exception.InsufficientBalanceException;
 
@@ -32,10 +33,13 @@ public class GiveHandler implements MessageHandler {
         try {
             int amount = Integer.parseInt(amountString);
             playersRepository.transferBalance(currentUser, targetUser, amount);
+            long discordId = message.getAuthor().map(User::getId).map(Snowflake::asLong).orElseThrow();
+            long balance = playersRepository.findByDiscordId(discordId).getBalance();
+            messageChannel.createEmbed(embedSpec -> createEmbedForGiveBalance(embedSpec, amount, targetUser, balance)).block();
         } catch (NumberFormatException e) {
-            messageChannel.createMessage(String.format("'%s' is not a valid amount", amountString)).block();
+            messageChannel.createEmbed(embedSpec -> createEmbedForInvalidAmount(embedSpec, amountString)).block();
         } catch (InsufficientBalanceException e) {
-            messageChannel.createMessage(String.format("you tried to give '%s' but you only have '%s'", e.getAmount() , e.getCurrentBalance())).block();
+            messageChannel.createEmbed(embedSpec -> createEmbedForInsufficientBalance(embedSpec, e.getAmount(), e.getCurrentBalance())).block();
         }
     }
 
@@ -44,4 +48,17 @@ public class GiveHandler implements MessageHandler {
         String lowerCaseMessage = message.getContent().toLowerCase();
         return lowerCaseMessage.startsWith(PREFIX + "bal give") || lowerCaseMessage.startsWith(PREFIX + "balance give");
     }
+
+    public void createEmbedForGiveBalance(EmbedCreateSpec embedCreateSpec, int amount, Long targetUser, Long  balance){
+        embedCreateSpec.addField( "Balance Transfer", String.format("You have given ** %s Quid** <:Quid:874742378014064750> to %s \n Your new balance is ** %s Quid** <:Quid:874742378014064750> ",  amount, targetUser , balance), true);
+    }
+
+    public void createEmbedForInvalidAmount(EmbedCreateSpec embedCreateSpec, String amountString){
+        embedCreateSpec.addField( "Insufficient Funds", String.format("'%s' is not a valid amount", amountString), true);
+    }
+
+    public void createEmbedForInsufficientBalance(EmbedCreateSpec embedCreateSpec, int amount, Long balance){
+        embedCreateSpec.addField( "Insufficient Funds", String.format("You tried to give a player** %s Quid** <:Quid:874742378014064750> but you only have **%s Quid** <:Quid:874742378014064750>",  amount , balance), true);
+    }
 }
+
